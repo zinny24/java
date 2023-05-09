db1e0e2c5d2ba72205f5228b53e1463ddf3c62d2
zinny24 <61670559+zinny24@users.noreply.github.com>
2023-05-09 10:40:45 +0900

    Add files via upload

---
+++ v4Exercise/Ex01.java
@@ -0,0 +1,13 @@
+package ascii;
+
+public class AsciiValue {
+
+	public static void main(String[] args) {
+		//A ~ Z까지 출력하시오
+		//for
+		for(int i = 65; i < 91; ++i) {
+			System.out.print((char)i);
+		}
+	}
+
+}
